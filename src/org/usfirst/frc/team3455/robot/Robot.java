eb86919356a84d45f7612913f4cd7ae6a6ec2b3f
tjhsstfrc <tjhsstfrc@gmail.com>
2017-02-13 17:58:02 -0500

    Add encoders, computer vision, drive code, and test autonomous

--- src/org/usfirst/frc/team3455/robot/Robot.java
+++ src/org/usfirst/frc/team3455/robot/Robot.java
@@ -1,190 +1,196 @@
 package org.usfirst.frc.team3455.robot;
 
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.RobotDrive.MotorType;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.Encoder;
 
 /**
- * This is a demo program showing how to use Mecanum control with the RobotDrive class.
+ * This is a demo program showing the use of the CameraServer class. With start
+ * automatic capture, there is no opportunity to process the image. Look at the
+ * IntermediateVision sample for how to process the image before sending it to
+ * the FRC PC Dashboard.
  */
-public class Robot extends SampleRobot {
+
+public class Robot extends IterativeRobot {
 	
-    RobotDrive robotDrive;
-    Joystick driveStick;
-    Joystick chainStick;
+	Encoder encoder;
+	NetworkTable table;
+	double data = 0.0;
 
-    // Channels for the wheels
-//    final int frontLeftChannel	= 4;
-//    final int rearLeftChannel	= 2;
-//    final int frontRightChannel	= 3;
-//    final int rearRightChannel	= 1;
-    
-    Talon frontLeft;
-    Talon frontRight;
-    Talon backLeft;
-    Talon backRight;
-    Talon chainCim1;
-    Talon chainCim2;
-    
-    Encoder chainEncoder;
-    
-    // The channel on the driver station that the joystick is connected to
-    final int driveJoystickChannel	= 0;
-    final int chainJoystickChannel = 1;
-    
-    int autoCount = 0;
-    double drivePowerMultiplier = 1.0;
+	//RobotDrive myDrive;
+	Joystick first, second;
+	
+	// Channels for the wheels
+	// final int frontLeftChannel = 4;
+	// final int rearLeftChannel = 2;
+	// final int frontRightChannel = 3;
+	// final int rearRightChannel = 1;
 
+	Talon frontLeft;
+	Talon frontRight;
+	Talon backLeft;
+	Talon backRight;
 
-    public Robot() {
-//    	robotDrive = new RobotDrive(frontLeftChannel, rearLeftChannel, frontRightChannel, rearRightChannel);
-//    	robotDrive.setExpiration(0.1);
-//        robotDrive.setInvertedMotor(MotorType.kFrontLeft, true);	// invert the left side motors
-//    	robotDrive.setInvertedMotor(MotorType.kRearLeft, true);		// you may need to change or remove this to match your robot
-    	frontLeft = new Talon(2);
-    	frontRight = new Talon(5);
-    	backLeft = new Talon(3);
-    	backRight = new Talon(4);
-    	chainCim1 = new Talon(0);
-    	chainCim2 = new Talon(1);
-        driveStick = new Joystick(driveJoystickChannel);
-        chainStick = new Joystick(chainJoystickChannel);
-        
-        chainEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k1X);
-    	//winchEncoder.setMaxPeriod(.1);
-    	chainEncoder.setMinRate(10);
-    	//winchEncoder.setDistancePerPulse(5);
-    	chainEncoder.setReverseDirection(true);
-    	chainEncoder.setSamplesToAverage(7);     
-    }
-    public void autonomous() {  
-    	autoCount = 0;
-        while (isAutonomous() && isEnabled()) {
-        	if(autoCount<30){
-            frontLeft.set(-.3);
-            frontRight.set(-.3);
-            backLeft.set(-.3);
-            backRight.set(-.3);
-            autoCount++;
-        	}
-        	else if(autoCount<65){
-        		frontLeft.set(.3);
-                frontRight.set(-.3);
-                backLeft.set(.3);
-                backRight.set(-.3);
-                autoCount++;
-        	}
-        	else if(autoCount<95){
-        		frontLeft.set(.3);
-                frontRight.set(.3);
-                backLeft.set(.3);
-                backRight.set(.3);
-                autoCount++;
-        	}
-        	else{
-        		frontLeft.set(0);
-        		frontRight.set(0);
-        		backLeft.set(0);
-        		backRight.set(0);
-        	}
-            Timer.delay(0.05);
-        }
-    } 
+	double scalingFactorEncoder = 2.5;
+	double errorFix = 0.05;
+	
+	double yAxis1;
+	double yAxis2;
+	double leftPower;
+	double rightPower;
+	
+	// update every 5 milliseconds
+	double kUpdatePeriod = 0.005;
 
-    /**
-     * Runs the motors with Mecanum drive.
-     */
-    public void operatorControl() {
-    	double ctrlThresh = .2;
-    	double minPower = .05;
-    	double maxPower = .75;
-    	double recip = (1 - ctrlThresh);
-    	double mult = (maxPower - minPower);
-    	double forward;
-    	double strafe;
-    	double rotate;
-    	double fAxis;
-    	double sAxis;
-    	double rAxis;
-    	double fDir;
-    	double sDir;
-    	double rDir;
-    	double frontLeftPower;
-    	double frontRightPower;
-    	double backLeftPower;
-    	double backRightPower;
-    	double chainPower;
-    
-    	
-        //robotDrive.setSafetyEnabled(false);
-        while (isOperatorControl() && isEnabled()) {
-        	
-        ctrlThresh = .2;
-   	     recip = 1-ctrlThresh;
-   	     fAxis = driveStick.getRawAxis(1);
-         sAxis = driveStick.getRawAxis(0);
-         rAxis = -1 * driveStick.getRawAxis(4);
+	public void robotInit() {
+		
+		table = NetworkTable.getTable("SmartDashboard");
+    	CameraServer.getInstance().startAutomaticCapture();
+		
+    	encoder = new Encoder(1, 2, true, EncodingType.k4X);
+		// CameraServer.getInstance().startAutomaticCapture();
+		/*
+		 * new Thread(() -> { UsbCamera camera =
+		 * CameraServer.getInstance().startAutomaticCapture();
+		 * camera.setResolution(640, 480);
+		 * 
+		 * CvSink cvSink = CameraServer.getInstance().getVideo(); CvSource
+		 * outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
+		 * 
+		 * Mat source = new Mat(); Mat output = new Mat();
+		 * 
+		 * while(!Thread.interrupted()) { cvSink.grabFrame(source);
+		 * outputStream.putFrame(source); } }).start();
+		 * NetworkTable.setServerMode(); NetworkTable.setTeam(3455); table =
+		 * NetworkTable.getTable("SmartDashboard");
+		 */
+    	encoder.setSamplesToAverage(5);
+    	encoder.setDistancePerPulse(1.0/360 * scalingFactorEncoder);
 
-   	     fDir = fAxis/Math.abs(fAxis);
-   	     sDir = sAxis/Math.abs(sAxis);
-   	     rDir = rAxis/Math.abs(rAxis);
-   	     
-   	     forward = 0;
-   	     rotate = 0;
-   	     strafe = 0;
-   	     
-   	     if(Math.abs(fAxis) > ctrlThresh)
-   		{forward = (fAxis-ctrlThresh*fDir)/recip;}
-   	     if(Math.abs(sAxis) > ctrlThresh)
-   		{strafe = (sAxis-ctrlThresh*sDir)/recip;}
-   	     if(Math.abs(rAxis) > ctrlThresh)
-   		{rotate = (rAxis-ctrlThresh*rDir)/recip;}
-              
-   	     
-   	     frontLeftPower = (-1*forward - rotate + strafe);
-   	     fDir = frontLeftPower/Math.abs(frontLeftPower);
-                frontLeftPower = (Math.abs(frontLeftPower*mult) + minPower) * fDir;
-               
-                frontRightPower = (forward - rotate + strafe);
-                fDir = frontRightPower/Math.abs(frontRightPower);
-                frontRightPower = (Math.abs(frontRightPower*mult) + minPower) * fDir;
+    	first = new Joystick(1);
+    	second = new Joystick(0);
 
-                backLeftPower = (-1*forward - rotate - strafe);
-                fDir = backLeftPower/Math.abs(backLeftPower);
-                backLeftPower = (Math.abs(backLeftPower*mult) + minPower) * fDir;
+		frontLeft = new Talon(5);
+		frontRight = new Talon(8);
+		backLeft = new Talon(6);
+		backRight = new Talon(7);
+		
+		frontLeft.setInverted(true);
+		backLeft.setInverted(true);
+		
+		
+	}
+	
+	public void autonomousInit() {
+		encoder.reset();
+//		
+	}
+	
+	public void autonomousPeriodic() {
+		table.putNumber("encoder", encoder.getDistance());
+		if(encoder.getDistance() < 1.0 - errorFix) {
+			tankDrive(-0.3, -0.3);
+		} else {
+			tankDrive(0.0, 0.0);
+		}
+	}
 
-                backRightPower = (forward - rotate - strafe);
-                fDir = backRightPower/Math.abs(backRightPower);
-                backRightPower = (Math.abs(backRightPower*mult) + minPower) * fDir;
+	public void teleopInit() {
+		encoder.reset();
+	}
 
-                frontRight.set(frontRightPower*drivePowerMultiplier);
-                frontLeft.set(frontLeftPower*drivePowerMultiplier);
-               backRight.set(backRightPower*drivePowerMultiplier);
-                backLeft.set(backLeftPower*drivePowerMultiplier); 
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		while (isOperatorControl() && isEnabled()) {
+			
+//			if(first.getRawButton(0)) {
+//				Thread cameraThread = new Thread(() -> {
+//					double turn = table.getNumber("Turning Value", 99.9);
+//					boolean turning = table.getBoolean("Turning", false);
+//			
+//					while(turning){
+//						frontLeft.set(turn);
+//						frontRight.set(-turn);
+//						backLeft.set(turn);
+//						backRight.set(-turn);
+//						turning = table.getBoolean("Turning", false);
+//						turn = table.getNumber("Turning Value", 99.9);
+//					}
+//		        });
+//				cameraThread.setName("cameraThread");
+//				
+//				if(getThreadByName("cameraThread") != null) {
+//					cameraThread.start();
+//				}
+//			}
+//			
+			
+			table.putNumber("encoder", encoder.getDistance());
+			yAxis1 = first.getRawAxis(1);
+			yAxis2 = first.getRawAxis(5);
+			table.putNumber("Y1", yAxis1);
+    		table.putNumber("Y2", yAxis2);
+    		tankDrive(yAxis1, yAxis2);
+    		Timer.delay(0.01);
+    		// wait to avoid hogging CPU cycles
+		}
+	}
 
-               chainPower = chainStick.getRawAxis(1);
-               if(Math.abs(chainPower) > .05){
-               chainCim1.set(-1*chainPower);
-               chainCim2.set(-1*chainPower);
-               }
-               
-               if(driveStick.getRawButton(2)){
-            	   
-            	   drivePowerMultiplier = 0.3;
-               }
-               if(driveStick.getRawButton(1)){
-            	   
-               drivePowerMultiplier = 1.0;
-               }
-        	
-            
-            Timer.delay(0.02);	// wait to avoid hogging CPU cycles
-        }
-    }
-    
+	public void tankDrive(double leftValue, double rightValue) {
+
+		// square the inputs (while preserving the sign) to increase fine
+		// control while permitting
+		// full power
+		leftValue = limit((leftValue));
+		rightValue = limit((rightValue));
+		
+		frontLeft.set(thresh(leftValue));
+		backLeft.set(thresh(leftValue));
+		frontRight.set(thresh(rightValue));
+		backRight.set(thresh(rightValue));
+		
+	}
+
+	protected static double limit(double num) {
+		if (num > 1.0) {
+			return 1.0;
+		}
+		if (num < -1.0) {
+			return -1.0;
+		}
+		return num;
+	}
+	
+	protected static double thresh(double num) {
+		if(num < 0.25 && num > -0.25) {
+			return 0.0;
+		}
+		return num;
+	}
+	
+	public Thread getThreadByName(String threadName) {
+	    for (Thread t : Thread.getAllStackTraces().keySet()) {
+	        if (t.getName().equals(threadName)) return t;
+	    }
+	    return null;
+	}
+
+	public void testPeriodic() {
+		
+	}
+
 }
